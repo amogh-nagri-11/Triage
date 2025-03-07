4b98055fc9889414fb79f86686319efe89f10ad7
Aryan-Pillai7 <aryanpx.007@gmail.com>
2025-03-07 10:09:39 +0530

    added database.py

---
+++ app/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+#Database URL (NO PASSWORD, as per your setup)
+DATABASE_URL = "postgresql://triage_user@localhost:5432/triage_db"
+
+engine = create_engine(DATABASE_URL)
+
+# Create a session factory
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Dependency to get a session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db  # Provides a database session
+    finally:
+        db.close()  # Closes session after use
